10075a96bc5d4cdfd3a378eead54e6ebf78d54e4
zhangyu26 <zhangyu26@inspur.com>
2023-07-11 14:41:01 +0800

    feat: 测试自定义日志starter

--- springboot-outside-config/src/main/java/com/zhangyu/controller/TestController.java
+++ springboot-outside-config/src/main/java/com/zhangyu/controller/TestController.java
@@ -3,11 +3,14 @@ package com.zhangyu.controller;
 import com.alibaba.fastjson2.JSON;
 import com.zhangyu.config.AuthorConfig;
 import com.zhangyu.domain.Author;
+import com.zhangyu.log.annotion.MyLog;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 /**
  * 测试controller
@@ -34,6 +37,35 @@ public class TestController {
     @Value("${loading-sequence:无数据}")
     private String sequence;
 
+    @Value("${bbbb.a1}")
+    private String custom;
+
+    /**
+     * 测试自定义starter
+     *
+     * @return
+     */
+    @MyLog
+    @GetMapping("/annotation/mylog")
+    public String testAnnotation() {
+        return "这是一条发生在" + LocalDateTime.now() + "的日志";
+    }
+
+    /**
+     * 测试自定义配置文件
+     *
+     * @return
+     */
+    @GetMapping("/custom")
+    public String testCustomProperties() {
+        return custom;
+    }
+
+    /**
+     * 测试配置文件加载顺序
+     *
+     * @return
+     */
     @GetMapping("/sequence")
     public String sequence() {
         return JSON.toJSONString(sequence);
